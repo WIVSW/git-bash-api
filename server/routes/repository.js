394d70cb642ca9217faa214099b71f8639c22add
WIVSW <olegspostnikov@gmail.com>
2019-09-13 04:08:25 +0500

    Add DELETE /api/repos/:repositoryId handler

--- server/routes/repository.js
+++ server/routes/repository.js
@@ -1,15 +1,31 @@
 const {promisify} = require('util');
 const exec = promisify(require('child_process').exec);
-// const fs = require('fs');
 const express = require('express');
 const router = express.Router();
-// const readdir = promisify(fs.readdir);
 
 router.post('/', async (req, res) => {
 	const {repositoryId, body: {url}} = req;
 	try {
 		await exec(
 			`cd ${process.env.PATH_TO_REPOS} && git clone ${url} ${repositoryId}`
+		);
+		res
+			.status(200)
+			.send({message: 'OK', data: [{
+				id: repositoryId,
+			}]});
+	} catch (error) {
+		res
+			.status(404)
+			.send({message: 'Not found', data: null});
+	}
+});
+
+router.delete('/', async (req, res) => {
+	const {repositoryId} = req;
+	try {
+		await exec(
+			`cd ${process.env.PATH_TO_REPOS} && rm -rf ${repositoryId}`
 		);
 		res
 			.status(200)
