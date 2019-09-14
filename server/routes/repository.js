259d186a18268801dae24912b520966486f7490f
user name <olegspostnikov@gmail.com>
2019-09-14 17:31:50 +0500

    Add handleRequest to repository routes

--- server/routes/repository.js
+++ server/routes/repository.js
@@ -2,45 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 const {download, remove} = require('../modules/repository-actions');
+const {handleRequest} = require('../modules/utils');
 
-const Response = require('../models/responses/response');
-const UnknownFail = require('../models/responses/unknown');
+router.post('/', handleRequest.bind(null, async (req) => {
+	const {repositoryId: id, body: {url}} = req;
+	await download(id, url);
+	return [{id}];
+}));
 
-router.post('/', async (req, res) => {
-	const {repositoryId, body: {url}} = req;
-	try {
-		await download(repositoryId, url);
-		res
-			.status(200)
-			.send({message: 'OK', data: [{
-				id: repositoryId,
-			}]});
-	} catch (error) {
-		const fail = error instanceof Response ? error : new UnknownFail();
-		const {message, data} = fail;
-		res
-			.status(fail.code)
-			.send({message, data});
-	}
-});
-
-router.delete('/', async (req, res) => {
-	const {repositoryId} = req;
-	try {
-		await remove(repositoryId);
-
-		res
-			.status(200)
-			.send({message: 'OK', data: [{
-				id: repositoryId,
-			}]});
-	} catch (error) {
-		const fail = error instanceof Response ? error : new UnknownFail();
-		const {message, data} = fail;
-		res
-			.status(fail.code)
-			.send({message, data});
-	}
-});
+router.delete('/', handleRequest.bind(null, async (req) => {
+	const {repositoryId: id} = req;
+	await remove(id);
+	return [{id}];
+}));
 
 module.exports = router;
