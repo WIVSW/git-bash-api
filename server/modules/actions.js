c836f028d0b578b1ae84f6e503371ef6a75a9004
user name <olegspostnikov@gmail.com>
2019-09-14 19:50:03 +0500

    Add cache for repository actions calls

--- server/modules/actions.js
+++ server/modules/actions.js
@@ -1,11 +1,19 @@
-const {getReposList} = require('../modules/repository-actions');
+const {
+	getReposList,
+	remove,
+	download,
+} = require('../modules/repository-actions');
 
 const Actions = {
 	READ_REPOS_LIST: 'read-repos-list',
+	REMOVE_REPO: 'remove-repo',
+	DOWNLOAD_REPO: 'download-repo',
 };
 
 const Handers = {};
 Handers[Actions.READ_REPOS_LIST] = getReposList;
+Handers[Actions.REMOVE_REPO] = remove;
+Handers[Actions.DOWNLOAD_REPO] = download;
 
 const memo = new Map();
 
@@ -35,6 +43,16 @@ const readReposList = async () => {
 	return await execute(Actions.READ_REPOS_LIST);
 };
 
+const removeRepo = async (...args) => {
+	return await execute(Actions.REMOVE_REPO, args);
+};
+
+const downloadRepo = async (...args) => {
+	return await execute(Actions.DOWNLOAD_REPO, args);
+};
+
 module.exports = {
 	readReposList,
+	removeRepo,
+	downloadRepo,
 };
--- server/routes/repository.js
+++ server/routes/repository.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const {download, remove} = require('../modules/repository-actions');
+const {removeRepo, downloadRepo} = require('../modules/actions');
 const {handleRequest} = require('../modules/utils');
 
 router.post('/', handleRequest.bind(null, async (req) => {
 	const {repositoryId: id, body: {url}} = req;
-	await download(id, url);
+	await downloadRepo(id, url);
 	return [{id}];
 }));
 
 router.delete('/', handleRequest.bind(null, async (req) => {
 	const {repositoryId: id} = req;
-	await remove(id);
+	await removeRepo(id);
 	return [{id}];
 }));
 
