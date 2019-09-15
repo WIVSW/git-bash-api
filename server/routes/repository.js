71ab66a67660d484d2ce814a17858f65a4492b4d
WIVSW <olegspostnikov@gmail.com>
2019-09-15 14:20:31 +0500

    Add files router

--- server/routes/repository.js
+++ server/routes/repository.js
@@ -3,9 +3,15 @@ const router = express.Router();
 
 const parseCommitHash = require('../middleware/parse-commit-hash');
 const commitsRoute = require('./commits');
+const filesRoute = require('./files');
 
 const {removeRepo, downloadRepo} = require('../modules/actions');
 const {handleRequest} = require('../modules/utils');
+
+
+router.get('/', (req, res) => {
+	res.redirect(`/api/repos/${req.repositoryId}/tree/master`);
+});
 
 router.post('/', handleRequest.bind(null, async (req) => {
 	const {repositoryId: id, body: {url}} = req;
@@ -20,5 +26,7 @@ router.delete('/', handleRequest.bind(null, async (req) => {
 }));
 
 router.use('/commits/:hash/', parseCommitHash, commitsRoute);
+
+router.use('/tree/:hash', parseCommitHash, filesRoute);
 
 module.exports = router;
