ad31b2f7c7499a5e6a627e290658ad0d5995a046
WIVSW <olegspostnikov@gmail.com>
2019-09-29 21:00:50 +0500

    Remove window usage

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,5 @@ import FilesList from './view/files-list';
 import Form from './view/form';
 
 const dic = new DIC();
-window.DIC = dic;
-window.FILES_LIST = new FilesList(dic.store);
-window.FORM = new Form(dic.store, dic.actions);
+new FilesList(dic.store);
+new Form(dic.store, dic.actions);
