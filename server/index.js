d8637fcedba5d8342bd98ac7d24db7bf34a10311
WIVSW <olegspostnikov@gmail.com>
2019-09-29 23:34:33 +0500

    Add prebuild task

--- server/index.js
+++ server/index.js
@@ -24,6 +24,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(express.static(resolve(__dirname, '../client/build')));
 app.use('/api', (req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers',
