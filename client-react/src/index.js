0e18066e7dc7783fe713546dfc6ef493b367c720
WIVSW <olegspostnikov@gmail.com>
2019-10-07 22:07:28 +0500

    Remove global varibles

--- client-react/src/index.js
+++ client-react/src/index.js
@@ -4,11 +4,8 @@ import { Provider } from 'react-redux';
 
 import './index.scss';
 import App from './App';
-import api from './api';
 import store from './redux/store';
 
-window.DIC = api;
-window.STORE = store;
 ReactDOM.render(
 	(
 		<Provider store={store}>
